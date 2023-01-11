b0ed8e0347820dc9c3982f3bf7d712b03fca02df
avery-stephens <astephens@student.neumont.edu>
2023-01-10 22:56:41 -0700

    Amongus agents are aware of surroundings
    they can see around and move towards the objects

---
+++ Assets/Scripts/AutomonousSussy/AutomonousSussy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomonousSussy : Agent
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var gameObjects = perception.GetGameObjects();
+        foreach(var gameObject in gameObjects)
+        {
+            Debug.DrawLine(transform.position, gameObject.transform.position);
+        }
+
+        if (gameObjects.Length > 0) 
+        {
+            Vector3 direction = (gameObjects[0].transform.position - transform.position).normalized;
+            movement.ApplyForce(direction * 2);
+        }
+
+        transform.position = Utilities.Wrap(transform.position, new Vector3(-10, -10, -10), new Vector3(10, 10, 10));
+    }
+}   
